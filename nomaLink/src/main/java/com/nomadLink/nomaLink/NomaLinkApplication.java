3b8ce5241974e36002c0a64ed3872e5a539feb89
Luis <lav.panter@hotmail.com>
2025-04-19 15:39:23 -0600

    stage zero

---
+++ nomaLink/src/main/java/com/nomadLink/nomaLink/NomaLinkApplication.java
@@ -0,0 +1,13 @@
+package com.nomadLink.nomaLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomaLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomaLinkApplication.class, args);
+	}
+
+}
---
+++ nomaLink/src/test/java/com/nomadLink/nomaLink/NomaLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomadLink.nomaLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomaLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
